6b5757f0190c41326ec102c60cc0a111b6d9f1e9
Yaroslav <zabaraaroslav@gmail.com>
2025-03-17 17:42:18 +0200

    Refactored MapViewApp to integrate Datasource and update car marker position on map. Improved map update mechanism.

--- MapView/main.py
+++ MapView/main.py
@@ -10,23 +10,41 @@ class MapViewApp(App):
     def __init__(self, **kwargs):
         super().__init__()
         # додати необхідні змінні
+        self.user_id: int = 1
+        self.mapview = MapView()
+        self.map_layer = LineMapLayer()
+        self.datasource: Datasource = Datasource(self.user_id)
+        self.car_marker = MapMarker(source="images/car.png")
 
     def on_start(self):
         """
         Встановлює необхідні маркери, викликає функцію для оновлення мапи
         """
+        self.mapview.add_marker(self.car_marker)
+        Clock.schedule_interval(self.update, 1)
 
     def update(self, *args):
         """
         Викликається регулярно для оновлення мапи
         """
-
+        points = self.datasource.get_new_points()
+        if not points:
+            return
+        for point in points:
+            coordinates = point.get_coordinates()
+            self.map_layer.add_point(coordinates)
+            self.update_car_marker(coordinates)
 
     def update_car_marker(self, point):
         """
         Оновлює відображення маркера машини на мапі
         :param point: GPS координати
         """
+        self.car_marker.lat = point[0]
+        self.car_marker.lon = point[1]
+
+        self.mapview.remove_marker(self.car_marker)
+        self.mapview.add_marker(self.car_marker)
 
     def set_pothole_marker(self, point):
         """
@@ -45,7 +63,7 @@ class MapViewApp(App):
         Ініціалізує мапу MapView(zoom, lat, lon)
         :return: мапу
         """
-        self.mapview = MapView()
+        self.mapview.add_layer(self.map_layer, mode="scatter")
         return self.mapview
 
 
