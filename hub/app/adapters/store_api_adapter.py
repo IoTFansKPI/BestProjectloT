12c15c2614b3052a692467b41338df5ccc23db52
Mykhailo Melnyk <mishamelnyk321@gmail.com>
2025-03-19 20:48:38 +0200

    add hub component

---
+++ hub/app/adapters/store_api_adapter.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import json
+from typing import List
+
+import pydantic_core
+import requests as req
+
+from app.entities.processed_agent_data import ProcessedAgentData
+from app.interfaces.store_api_gateway import StoreGateway
+from config import STORE_API_BASE_URL
+
+class StoreApiAdapter(StoreGateway):
+    def __init__(self, api_base_url):
+        self.api_base_url = api_base_url
+
+    def save_data(self, processed_agent_data_batch: List[ProcessedAgentData]):
+        # Make a POST request to the Store API endpoint with the processed data
+        data = json.dumps(
+            processed_agent_data_batch, default=pydantic_core.to_jsonable_python
+        )
+        
+        r = req.post(f"{STORE_API_BASE_URL}/processed_agent_data/", data)
+        
+        if r.status_code == 200:
+            return 0
+        else:
+            return {r.status_code, r.text}
+        pass
